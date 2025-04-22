ba6b0fee101be28cfa5a2f2a2f741c50c0e08a8c
Andresjbb99 <163828884+Andresjbb99@users.noreply.github.com>
2025-04-22 10:53:02 +0200

    Add files via upload

---
+++ DI_T1A1_BerrocalAndres/src/main/java/ieslosmontecillos/di_t1a1_berrocalandres/HelloController.java
@@ -0,0 +1,14 @@
+package ieslosmontecillos.di_t1a1_berrocalandres;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
